509adb822ff3a9198ef9be011917e9be1d56fb81
Nesquirt <sg5027@gmail.com>
2022-04-28 14:32:45 +0000

    kprofile:fix some error in compilation

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -56,7 +56,7 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
-  if (!READ_ONCE(b->df) || !test_bit(SCREEN_ON, &b->state) || active_mode() == 1)
+  if (!READ_ONCE(b->df) || !test_bit(SCREEN_OFF, &b->state) || active_mode() == 1)
     return;
 
   set_bit(INPUT_BOOST, &b->state);
@@ -78,7 +78,7 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (!READ_ONCE(b->df) || !test_bit(SCREEN_ON, &b->state) || active_mode() == 1)
+	if (!READ_ONCE(b->df) || !test_bit(SCREEN_OFF, &b->state) || active_mode() == 1)
 	  return;
 
 	do {
