c0484fac9c2526789955bde79cc520b44c494ab0
kuroringo90 <ssss9999@gmx.it>
2022-04-30 02:32:07 +0200

    zswap: enable zswap default

--- mm/zswap.c
+++ mm/zswap.c
@@ -74,8 +74,8 @@ static u64 zswap_duplicate_entry;
 
 #define ZSWAP_PARAM_UNSET ""
 
-/* Enable/disable zswap (disabled by default) */
-static bool zswap_enabled = 0;
+/* Enable/disable zswap (enable by default) */
+static bool zswap_enabled = 1;
 static int zswap_enabled_param_set(const char *,
 				   const struct kernel_param *);
 static const struct kernel_param_ops zswap_enabled_param_ops = {
